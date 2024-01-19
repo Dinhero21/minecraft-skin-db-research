9e183ee44ae9f1f5667bf77d937fdb979c5195ac
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2024-01-19 17:34:44 -0300

    alpha premultiplication
    I probably messed up the standard deviation but I have quintuple checked the code and it seems to be all good.
    
    I probably messed up the code before which is actually really cool as the standard deviation before was really ugly and cursed imo (inner layer).

--- src/analyze.ts
+++ src/analyze.ts
@@ -11,14 +11,22 @@ const SKIN_CHANNELS = 4
 const SKIN_PIXELS = SKIN_WIDTH * SKIN_HEIGHT
 const SKIN_BYTES = SKIN_PIXELS * SKIN_CHANNELS
 
+// TODO: Find a less horrendous way of sharing data like this between calculations
+const totalPixelAlpha: number[] = Array(SKIN_PIXELS).fill(0)
+
 async function calculateAverage (): Promise<number[]> {
   const total: number[] = Array(SKIN_BYTES).fill(0)
 
   const splitter = new MetaFileSplitter(SKIN_BYTES)
 
   splitter.on('data', buffer => {
-    for (let i = 0; i < buffer.length; i++) {
-      total[i] += buffer[i]
+    for (let i = 0; i < buffer.length; i += 4) {
+      const alpha = buffer[i + 3] / 256
+
+      total[i + 0] += buffer[i + 0] * alpha
+      total[i + 1] += buffer[i + 1] * alpha
+      total[i + 2] += buffer[i + 2] * alpha
+      total[i + 3] += buffer[i + 3]
     }
   })
 
@@ -26,19 +34,50 @@ async function calculateAverage (): Promise<number[]> {
 
   await new Promise<void>(resolve => splitter.stream.on('end', resolve))
 
-  const average = total.map(n => n / splitter.files)
+  for (let i = 0; i < totalPixelAlpha.length; i++) {
+    const alpha = total[(i * 4) + 3] / 256
+
+    totalPixelAlpha[i] = alpha
+  }
+
+  const average = Array.from(total)
+
+  for (let iPixel = 0; iPixel < totalPixelAlpha.length; iPixel++) {
+    const iByte = iPixel * 4
+
+    const alpha = totalPixelAlpha[iPixel]
+
+    average[iByte + 0] /= alpha
+    average[iByte + 1] /= alpha
+    average[iByte + 2] /= alpha
+    average[iByte + 3] /= splitter.files
+  }
 
   return average
 }
 
 async function calculateStandardDeviation (average: number[]): Promise<number[]> {
-  const squareStandardDeviation: number[] = Array(SKIN_BYTES).fill(0)
+  const squareStandardDeviation: number[] = Array(SKIN_PIXELS * 3).fill(0)
 
   const splitter = new MetaFileSplitter(SKIN_BYTES)
 
   splitter.on('data', buffer => {
-    for (let i = 0; i < buffer.length; i++) {
-      squareStandardDeviation[i] += Math.pow(buffer[i] - average[i], 2)
+    for (let iPixel = 0; iPixel < SKIN_PIXELS; iPixel++) {
+      const iByteRGBA = iPixel * 4
+
+      const alpha = buffer[iByteRGBA + 3] / 256
+
+      const totalAlpha = totalPixelAlpha[iPixel]
+
+      const r = buffer[iByteRGBA + 0] * alpha / totalAlpha
+      const g = buffer[iByteRGBA + 1] * alpha / totalAlpha
+      const b = buffer[iByteRGBA + 2] * alpha / totalAlpha
+
+      const iByteRGB = iPixel * 3
+
+      squareStandardDeviation[iByteRGB + 0] += Math.pow(r - average[iByteRGBA + 0], 2)
+      squareStandardDeviation[iByteRGB + 1] += Math.pow(g - average[iByteRGBA + 1], 2)
+      squareStandardDeviation[iByteRGB + 2] += Math.pow(b - average[iByteRGBA + 2], 2)
     }
   })
 
@@ -47,6 +86,8 @@ async function calculateStandardDeviation (average: number[]): Promise<number[]>
   await new Promise<void>(resolve => splitter.stream.on('end', resolve))
 
   const standardDeviation = squareStandardDeviation.map(Math.sqrt)
+
+  console.log(standardDeviation)
 
   return standardDeviation
 }
@@ -78,32 +119,29 @@ await sharp(standardDeviationBuffer, {
   raw: {
     width: SKIN_WIDTH,
     height: SKIN_HEIGHT,
-    channels: SKIN_CHANNELS
+    channels: 3
   }
 })
-  .toFile('./standard-deviation.png')
-
-await sharp(standardDeviationBuffer, {
-  raw: {
-    width: SKIN_WIDTH,
-    height: SKIN_HEIGHT,
-    channels: SKIN_CHANNELS
-  }
-})
-  .removeAlpha()
   .toFile('./standard-deviation.rgb.png')
 
+const standardDeviationBufferRGBA = Buffer.allocUnsafe(SKIN_BYTES)
+
 // α(average) -> α(standard deviation)
-for (let i = 0; i < SKIN_BYTES; i += 4) {
-  standardDeviationBuffer[i + 3] = averageBuffer[i + 3]
+for (let iPixel = 0; iPixel < SKIN_PIXELS; iPixel++) {
+  const iByteRGBA = iPixel * 4
+  const iByteRGB = iPixel * 3
+
+  standardDeviationBufferRGBA[iByteRGBA + 0] = standardDeviationBuffer[iByteRGB + 0]
+  standardDeviationBufferRGBA[iByteRGBA + 1] = standardDeviationBuffer[iByteRGB + 1]
+  standardDeviationBufferRGBA[iByteRGBA + 2] = standardDeviationBuffer[iByteRGB + 2]
+  standardDeviationBufferRGBA[iByteRGBA + 3] = averageBuffer[iByteRGBA + 3]
 }
 
-await sharp(standardDeviationBuffer, {
+await sharp(standardDeviationBufferRGBA, {
   raw: {
     width: SKIN_WIDTH,
     height: SKIN_HEIGHT,
-    channels: SKIN_CHANNELS
+    channels: 4
   }
 })
-  .removeAlpha()
   .toFile('./standard-deviation.average-alpha.png')
